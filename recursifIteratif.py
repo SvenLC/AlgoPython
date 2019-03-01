503dee5c6f34019f8d22a5395e85c1f8931cd3a2
svenl <sven.lecann@live.fr>
2019-03-01 11:01:46 +0100

    boucle

---
+++ recursifIteratif.py
@@ -0,0 +1,35 @@
+# Récursif
+def ajouterUn(x, y):
+    if x == y:
+        return x
+    else:
+        return ajouterUn(x+1, y)
+
+print(ajouterUn(1, 10))
+
+# Itératif boucle for
+def ajouterDeux(x, y):
+    for i in range(0,10,1):
+        x += 2
+    
+    return x
+
+print(ajouterDeux(1,10))
+
+# Itératif boucle while
+listeNbPair = []
+i = 1
+
+def estPair(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+while len(listeNbPair) <= 10 :    
+    if estPair(i):
+        listeNbPair.append(i)
+    i += 1
+
+print(listeNbPair)

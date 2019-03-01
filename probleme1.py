3fa8604b1073794563e3795b6db505748a2b1ca8
svenl <sven.lecann@live.fr>
2019-03-01 12:01:08 +0100

    Problème 1 correction

---
+++ probleme1.py
@@ -0,0 +1,12 @@
+# Je remplis une tirelire de la manière suivante:
+# - Je commence par déposer 1€ dans la tirelire (un lundi)
+# - Puis j'y dépose 2,01 € le jour suivant (un mardi)
+# - Puis 3,02 € le jour suivant (un mercredi)
+# - Puis 4,03 € le jour suivant (un jeudi)
+# - etc, (chaque jou j'y dépose 1,01 de plus que la veille).
+#     par exemple : le huitième jour (un lundi), j'y dépose 8,07 € et le contenu total
+#     de la tirelire est : 1 + 2,01 + 3,02 + 4,03 + 5,04 + 6,05 + 7,06 + 8,07 = 36,28 €
+
+# Lorsque le contenu total de la tirelire dépasse 1500 €, je casse la tirelire.
+
+# Le but du sujet est de déterminer le contenu de la tirelire lorsque je la casserai.
